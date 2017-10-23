fde479e2dbc01fe8ef441f30c55d800bcffe9a7c
ZeroOneSummer <592706328@qq.com>
2017-10-23 13:47:52 +0800

    md5加密工具类

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,36 +4,17 @@ import org.apache.ibatis.session.SqlSession;
 
 import mapper.user.UserMapper;
 import pojo.User;
+import utils.H5Utils;
 import utils.MyBatisUtil;
 
 public class Test {
 	public static void main(String[] args) {
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		SqlSession session=  MyBatisUtil.createSqlSession();		
-	
+		/*SqlSession session=  MyBatisUtil.createSqlSession();			
 		User user= session.getMapper(UserMapper.class).getUsers();
-		System.out.println(user.getLoginName()+"---"+user.getPassword());
-
-
-		System.out.println("测试提交>>10.23");
-		System.out.println("测试第二次提交>>10.23");
-		System.out.println("测试提交yan");
+		System.out.println(user.getLoginName()+"---"+user.getPassword());*/
 		
-		
+		String pwd = H5Utils.Hex5("123456789");
+		System.out.println(pwd);
 	}
 }
