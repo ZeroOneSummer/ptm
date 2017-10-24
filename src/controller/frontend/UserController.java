919320248dcd23ed85e38808804971bd1cc52855
qq357632941 <357632941@qq.com>
2017-10-24 19:11:18 +0800

    1024提交代码-tao

---
+++ src/controller/frontend/UserController.java
@@ -0,0 +1,16 @@
+package controller.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 用户操作
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+    
+}
