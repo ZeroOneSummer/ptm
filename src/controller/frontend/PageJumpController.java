394469459918f05ecce73846b285de9ce4f55307
gavin-guocai <guocai.wu@qq.com>
2017-10-31 13:14:36 +0800

    系统页面跳转已完成大部分

--- src/controller/frontend/PageJumpController.java
+++ src/controller/frontend/PageJumpController.java
@@ -235,4 +235,60 @@ public class PageJumpController {
 
 	}
 	
+
+	/**
+	 * 跳转到-新手福利-页面
+	 * @return
+	 */
+	@RequestMapping("/newfuli")
+	public String toNewfuli(){
+		return "frontend/invest/Newfuli";
+	}
+	
+	
+	/**
+	 * 跳转到-公告中心-页面
+	 * @return
+	 */
+	@RequestMapping("/phoneNews.html")
+	public String toPhoneNews(){
+		
+		return "frontend/helpCenter/phoneNews";
+	}
+	
+	
+	/**
+	 * 跳转到-媒体-页面
+	 * @return
+	 */
+	@RequestMapping("/Meiti.html")
+	public String toMeiti(){
+		
+		return "frontend/infoCenter/Meiti";
+	}
+	
+	
+	/**
+	 * 跳转到-实名认证-页面
+	 * @return
+	 */
+	@RequestMapping("/realName.html")
+	public String toRealName(){
+		
+		return "frontend/personalCenter/realName";
+	}
+	
+	
+	/**
+	 * 跳转到-绑定银行卡-页面
+	 * @return
+	 */
+	@RequestMapping("/BindBack.html")
+	public String toBindBack(){
+		
+		return "frontend/personalCenter/BindBack";
+	}
+	
+	
+	
 }
